6d7b682b73334e264eedd63634897c91137ec9c3
KUMAR <nitesh.f.kumar@capgemini.com>
2024-10-28 17:35:01 +0530

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
-fetch("JavaCodingQuestions/Split Array into the Subsets.md").then(response => response.text())
-.then(markdown => {
-    document.getElementById('markdown-content').innerHTML = marked.parse(markdown);
-});
+// Load and display the Markdown file
+fetch('JavaCodingQuestions/Split Array into the Subsets.md')  // Update the path to the Markdown file
+    .then(response => response.text())
+    .then(markdown => {
+        const html = marked(markdown);
+        document.getElementById('markdown-content').innerHTML = html;
+    })
+    .catch(error => console.error('Error loading Markdown file:', error));
